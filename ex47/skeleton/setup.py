ab6c25b4dc2c54a1e3985305688e3bc30c75d0f9
Shen Zhu <shenzhu0127@gmail.com>
2015-05-11 10:26:11 +0800

    ex47

---
+++ ex47/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup 
+except ImportError:
+	from distutils.core import setup
+
+config = [
+          'description':'My Projcet',
+          'author':'Shen Zhu',
+          'url':'URL to get it at.',
+          'download_url':'Where to download it.',
+          'author_email':'shenzhu0127@outlook.com',
+          'version':'0.1',
+          'install_requires':['nose'],
+          'packages':['ex47'],
+          'scripts':[],
+          'name':'ex47'
+         ]
+
+setup(**config)
